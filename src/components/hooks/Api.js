6dbcd428660abb73c47d81c182e111dacb6e0273
amanarustu1-prog <amanarustu1@gmail.com>
2025-12-18 10:14:11 +0530

    New-changes

---
+++ src/components/hooks/Api.js
@@ -0,0 +1,9 @@
+import axios from "../../interceptors/Axios";
+
+export const fetch_get_data = async (url, postdata) => {
+    try{
+        
+    }catch(error){
+        console.log(error);
+    }
+}
